57f726b62528be00a13bb5a32727f9f15b2414f3
cm-uu <3034679649@qq.com>
2026-03-12 15:27:56 +0800

    new feature:data-graph

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -2,7 +2,7 @@
 API 路由导出
 """
 from fastapi import APIRouter
-from app.api import auth, courseware, chat, knowledge, libraries
+from app.api import auth, courseware, chat, knowledge, libraries, data_analysis
 from app.api import html_upload, html_chat, html_export
 
 api_router = APIRouter()
@@ -12,6 +12,7 @@ api_router.include_router(courseware.router)
 api_router.include_router(chat.router)
 api_router.include_router(knowledge.router)
 api_router.include_router(libraries.router)
+api_router.include_router(data_analysis.router)
 
 api_router.include_router(html_upload.router, prefix="/html", tags=["html"])
 api_router.include_router(html_chat.router, prefix="/html", tags=["html"])
