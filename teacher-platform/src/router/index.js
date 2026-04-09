517f9c9cb64504575db50847ec0f1ebd29f9972d
zzcn <zzcn.leo@qq.com>
2026-04-09 21:09:07 +0800

    feat(rehearsal): register rehearsal routes

--- teacher-platform/src/router/index.js
+++ teacher-platform/src/router/index.js
@@ -77,6 +77,24 @@ const routes = [
     component: () => import('../views/admin/AdminSystemLogs.vue'),
     meta: { requiresAuth: true, layout: 'nav' }
   },
+  {
+    path: '/rehearsal/new',
+    name: 'RehearsalNew',
+    component: () => import('../views/rehearsal/RehearsalNew.vue'),
+    meta: { requiresAuth: true, layout: 'nav' }
+  },
+  {
+    path: '/rehearsal/play/:id',
+    name: 'RehearsalPlay',
+    component: () => import('../views/rehearsal/RehearsalPlay.vue'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/rehearsal/history',
+    name: 'RehearsalHistory',
+    component: () => import('../views/rehearsal/RehearsalHistory.vue'),
+    meta: { requiresAuth: true, layout: 'nav' }
+  },
   { path: '/:pathMatch(.*)*', redirect: '/' }
 ]
 
